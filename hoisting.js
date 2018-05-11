5cc7627168957b74e26d3f1cd8f1246ac8a3826b
Aranya Gaddam <agaddam1@gmail.com>
2018-05-11 02:05:00 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -21,13 +21,13 @@ function crazy() {
 
 function sayMyName() {
   // fix the code in here:
-
+  var name = "Cricky";
 
   sayMy();
 
   function sayMy() {
     console.log(name);
-    var name = "Kristin";
+     name = "Kristin";
   }
 
 }
